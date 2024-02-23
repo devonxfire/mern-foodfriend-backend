ff57604c1d13d0a783e47783a1a9c5b98e7782dd
devonmartindale <devonmartindale@gmail.com>
2024-02-23 11:15:59 +0200

    created api route and logic to create a restaurant

--- src/index.ts
+++ src/index.ts
@@ -3,22 +3,31 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import MyUserRoute from "./routes/MyUserRoute";
+import MyRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
   .then(() => console.log("Connected to MongoDB"));
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.get("/health", async (req: Request, res: Response) => {
   res.send({ message: "health OK!" });
 });
 
-// Routes
 app.use("/api/my/user", MyUserRoute);
+app.use("/api/my/restaurant", MyRestaurantRoute);
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");

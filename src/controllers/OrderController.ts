1ded699a71377bfa384aa35e51e2761412292f91
devonmartindale <devonmartindale@gmail.com>
2024-03-12 14:30:08 +0200

    creating stripe api

---
+++ src/controllers/OrderController.ts
@@ -0,0 +1,116 @@
+import { Request, Response } from "express";
+import Stripe from "stripe";
+import Restaurant, { MenuItemType } from "../models/restaurant.model";
+
+const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string);
+const FRONTEND_URL = process.env.FRONTEND_URL as string;
+
+type CheckoutSessionRequest = {
+  cartItems: {
+    menuItemId: string;
+    quantity: number;
+    name: string;
+  }[];
+  deliveryDetails: {
+    email: string;
+    name: string;
+    addressLine1: string;
+    city: string;
+  };
+  restaurantId: string;
+};
+
+const createCheckoutSession = async (req: Request, res: Response) => {
+  try {
+    const checkoutSessionRequest: CheckoutSessionRequest = req.body;
+    const restaurant = await Restaurant.findById(
+      checkoutSessionRequest.restaurantId
+    );
+    if (!restaurant) {
+      return res.status(404).json({ message: "Restaurant not found" });
+    }
+
+    const lineItems = createLineItems(
+      checkoutSessionRequest,
+      restaurant.menuItems
+    );
+
+    const session = await createSession(
+      lineItems,
+      "TEST_ORDER_ID",
+      restaurant.deliveryPrice,
+      restaurant._id.toString()
+    );
+    if (!session.url) {
+      return res
+        .status(500)
+        .json({ message: "Failed to create stripe session" });
+    }
+    res.json({ url: session.url });
+  } catch (error: any) {
+    console.log(error);
+    res.status(500).json({ message: error.raw.message });
+  }
+};
+const createLineItems = (
+  checkoutSessionRequest: CheckoutSessionRequest,
+  menuItems: MenuItemType[]
+) => {
+  const lineItems = checkoutSessionRequest.cartItems.map((cartItem) => {
+    const menuItem = menuItems.find(
+      (item) => item._id.toString() === cartItem.menuItemId
+    );
+
+    if (!menuItem) {
+      throw new Error("Menu item not found");
+    }
+
+    const line_item: Stripe.Checkout.SessionCreateParams.LineItem = {
+      price_data: {
+        currency: "zar",
+        unit_amount: menuItem.price,
+        product_data: {
+          name: menuItem.name,
+        },
+      },
+      quantity: cartItem.quantity,
+    };
+    return line_item;
+  });
+
+  return lineItems;
+};
+
+const createSession = async (
+  lineItems: Stripe.Checkout.SessionCreateParams.LineItem[],
+  orderId: string,
+  deliveryPrice: number,
+  restaurantId: string
+) => {
+  const sessionData = await STRIPE.checkout.sessions.create({
+    line_items: lineItems,
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          display_name: "Delivery",
+          type: "fixed_amount",
+          fixed_amount: {
+            amount: deliveryPrice,
+            currency: "zar",
+          },
+        },
+      },
+    ],
+    mode: "payment",
+    metadata: {
+      orderId,
+      restaurantId,
+    },
+    success_url: `${FRONTEND_URL}/order-status?success=true`,
+    cancel_url: `${FRONTEND_URL}/detail/${restaurantId}?cancelled=true`,
+  });
+
+  return sessionData;
+};
+
+export default { createCheckoutSession };
